342fa1d51b7235d95f705cab7e353d1613c1059f
Gergelj Kis <gergelj.kis@databricks.com>
2026-09-21 15:12:40 +0200

    ncc auto approve endpoints

--- terraform/azure_serverless/outputs.tf
+++ terraform/azure_serverless/outputs.tf
@@ -1,19 +1,3 @@
 output "workspace_url" {
   value = "https://${module.databricks.databricks_workspace_url}"
 }
-
-output "databricks_id" {
-  value = module.databricks.databricks_id
-}
-
-output "databricks_workspace_id" {
-  value = module.databricks.databricks_workspace_id
-}
-
-output "resource" {
-  value = module.databricks.resource
-}
-
-output "resource_id" {
-  value = module.databricks.resource_id
-}
